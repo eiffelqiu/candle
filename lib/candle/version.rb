cece777c43e22052b5a8ae28678636be3f841dd6
Eiffel Qiu <eiffelqiu@gmail.com>
2013-03-06 20:45:43 +0800

    fix double VERSION

--- lib/candle/version.rb
+++ lib/candle/version.rb
@@ -1,9 +1,7 @@
 module Candle
   module Version
-    MAJOR = 0
-    MINOR = 0
-    PATCH = 4
-    
+    VERSIONSTRING = File.read(File.join(File.dirname(__FILE__),'/../../VERSION'))
+    MAJOR, MINOR, PATCH = VERSIONSTRING.split('.')
     STRING = [MAJOR, MINOR, PATCH].compact.join('.').chomp('.')
   end
 end
